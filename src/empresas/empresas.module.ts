0926f245993161a80816edd1acac90b92ea84789
Ewerton <ewerton.lfb@gmail.com>
2025-08-28 10:59:06 -0300

    melhoria no modulo de empresas

--- src/empresas/empresas.module.ts
+++ src/empresas/empresas.module.ts
@@ -1,10 +1,40 @@
 // empresas/empresas.module.ts
 import { Module } from '@nestjs/common';
-import { EmpresasController } from './empresas.controller';
-import { EmpresasService } from './empresas.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { EmpresaCategoriaController } from './controllers/categoria.controller';
+import { EmpresaSistemaController } from './controllers/empresa-sistema.controller';
+import { EmpresaTipoController } from './controllers/empresa-tipo.controller';
+import { EmpresasController } from './controllers/empresas.controller';
+import { SistemasController } from './controllers/sistemas.controller';
+import { EmpresaCategoriaService } from './services/categoria.service';
+import { EmpresaSistemaService } from './services/empresa-sistema.service';
+import { EmpresaTipoService } from './services/empresa-tipo.service';
+import { EmpresasService } from './services/empresas.service';
+import { SistemasService } from './services/sistemas.service';
+
 
 @Module({
-  controllers: [EmpresasController],
-  providers: [EmpresasService],
+  imports: [PrismaModule],
+  controllers: [
+    EmpresasController,
+    EmpresaCategoriaController,
+    EmpresaTipoController,
+    EmpresaSistemaController,
+    SistemasController
+  ],
+  providers: [
+    EmpresasService,
+    EmpresaCategoriaService,
+    EmpresaTipoService,
+    EmpresaSistemaService,
+    SistemasService
+  ],
+  exports: [
+    EmpresasService,
+    EmpresaCategoriaService,
+    EmpresaTipoService,
+    EmpresaSistemaService,
+    SistemasService
+  ],
 })
 export class EmpresasModule {}
