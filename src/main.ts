8c950a7f5e33000bcbc0184d596a0a92c667c088
Ewerton <ewerton.lfb@gmail.com>
2025-09-02 00:13:21 -0300

    problema cookie parse

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
-import * as net from 'net';
+import cookieParser from 'cookie-parser';
+
+// import * as net from 'net';
+import net from 'net';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
 import { BigIntInterceptor } from './common/interceptors/bigint.interceptor';
