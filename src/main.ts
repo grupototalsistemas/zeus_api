1c34786b59b54ec414ea18be5b36c9d5c10ca902
Yann Freitas <ramires.luan@gmail.com>
2025-09-02 18:33:24 -0300

    fixando origin pra cors

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
 
   // CORS para cookies
   app.enableCors({
-    origin: true, // Permite qualquer origem
+    origin: 'https://zeus-front-swart.vercel.app', // Permite apenas o front-end
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
