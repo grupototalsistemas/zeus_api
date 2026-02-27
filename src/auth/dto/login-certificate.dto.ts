9f9b8f09cc4fe2a01420d4152bc8564d67a5f4f9
ewertho <ewerton.lfb@gmail.com>
2026-02-27 10:43:42 -0300

    retirada de login com certificado do zeus

--- src/auth/dto/login-certificate.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class LoginCertificateDto {
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
-  certificate: string;
-}
